e2d25c386c024bab50c393bc1512337ebb7b12ee
Aperture Apps <apertureapps@gmail.com>
2012-09-16 23:21:04 -0400

    Added email to Submit screen

--- frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/SubmitScreenViewController.h
+++ frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/SubmitScreenViewController.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
 
-@interface SubmitScreenViewController : UIViewController
+@interface SubmitScreenViewController : UIViewController<MFMailComposeViewControllerDelegate>
+
+- (IBAction)buttonBackButtonPressed:(id)sender;
 
 - (IBAction)buttonSubmitButtonPressed:(id)sender;
+
+@property (strong, nonatomic) IBOutlet UITextField *textFieldBusinessName;
+
+@property (strong, nonatomic) IBOutlet UITextField *textFieldTwitterHandle;
+
+@property (strong, nonatomic) IBOutlet UITextField *textFieldEmailAddress;
 
 @end
--- frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/SubmitScreenViewController.m
+++ frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/SubmitScreenViewController.m
@@ -13,6 +13,9 @@
 @end
 
 @implementation SubmitScreenViewController
+@synthesize textFieldBusinessName;
+@synthesize textFieldTwitterHandle;
+@synthesize textFieldEmailAddress;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,6 +34,9 @@
 
 - (void)viewDidUnload
 {
+	[self setTextFieldBusinessName:nil];
+	[self setTextFieldTwitterHandle:nil];
+	[self setTextFieldEmailAddress:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -40,8 +46,98 @@
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (IBAction)buttonSubmitButtonPressed:(id)sender
+
+- (IBAction)buttonBackButtonPressed:(id)sender
 {
 	[self.navigationController popViewControllerAnimated:YES];
+}
+
+- (IBAction)buttonSubmitButtonPressed:(id)sender
+{
+	// allocatind new message composer window
+    MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+	
+    // setting a delegate method to "self"
+    mc.mailComposeDelegate = self;
+	
+    // pre-populating the message subject
+    [mc setSubject:@"Send me a message"];
+	
+    // adding content of the message as a plain text
+    [mc setMessageBody:@"Send me a message is you like this tutorial :)" isHTML:NO];
+	
+    // adding content of the message as an HTML
+	
+	
+	
+    [mc setMessageBody:@"<p>Send me a message is you like this tutorial :)<p>" isHTML:YES];
+	
+    // adding recipients
+    [mc setToRecipients:[NSArray arrayWithObjects:@"Das <das@leviait.com>", @"arunabhdas@gmail.com", nil]];
+	
+    // adding recipients for a send copy to (arrayWithObject or arrayWithObjects)
+    [mc setCcRecipients:[NSArray arrayWithObject:@"das@leviait.com"]];
+	
+    // adding hidden recipients
+    [mc setBccRecipients:[NSArray arrayWithObject:@"fivemillivolts@gmail.com"]];
+	
+    // adding image attachment
+    // getting path for the image we have in the tutorial project
+    // NSString *path = [[NSBundle mainBundle] pathForResource:@"Extra_Xcode_100x100" ofType:@"png"];
+	
+    // loading content of the image into NSData
+    // NSData *imageData = [NSData dataWithContentsOfFile:path];
+	
+    // adding the attachment to he message
+    // [mc addAttachmentData:imageData mimeType:@"image/png" fileName:@"Collection"];
+	
+    // setting different than the default transition for the modal view controller
+    [mc setModalTransitionStyle:UIModalTransitionStyleFlipHorizontal];
+	
+    /*
+     Modal view controllers transitions:
+	 
+     UIModalTransitionStyleCoverVertical => pops up from the bottom, default transition
+     UIModalTransitionStyleCrossDissolve => fade on the screen
+     UIModalTransitionStyleFlipHorizontal => page flip
+     */
+	
+    // displaying our modal view controller on the screen (of course animated has to be set on YES if you want to see any transition)
+    [self presentModalViewController:mc animated:YES];
+
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    // switchng the result
+    switch (result) {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail send canceled.");
+            /*
+             Execute your code for canceled event here ...
+             */
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved.");
+            /*
+             Execute your code for email saved event here ...
+             */
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent.");
+            /*
+             Execute your code for email sent event here ...
+             */
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail send error: %@.", [error localizedDescription]);
+            /*
+             Execute your code for email send failed event here ...
+             */
+            break;
+        default:
+            break;
+    }
+    // hide the modal view controller
+    [self dismissModalViewControllerAnimated:YES];
 }
 @end
