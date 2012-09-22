d1905ac3b3dd5d5220030bc7e94d50e16b3e5dff
Aperture Apps <apertureapps@gmail.com>
2012-09-22 01:17:14 -0400

    Silencing deals found label

---
+++ frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/RequestCityViewController.h
@@ -0,0 +1,13 @@
+//
+//  RequestCityViewController.h
+//  DealsNearMeApp
+//
+//  Created by Das on 9/22/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RequestCityViewController : UIViewController
+
+@end
---
+++ frontend/iOSApp/DealsNearMeApp/DealsNearMeApp/RequestCityViewController.m
@@ -0,0 +1,43 @@
+//
+//  RequestCityViewController.m
+//  DealsNearMeApp
+//
+//  Created by Das on 9/22/12.
+//
+//
+
+#import "RequestCityViewController.h"
+
+@interface RequestCityViewController ()
+
+@end
+
+@implementation RequestCityViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
